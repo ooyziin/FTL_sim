4b575d840741908412b27dbe670422a804c790f4
ooyziin <2002byeon@naver.com>
2025-08-18 23:09:31 +0900

    Add files via upload

---
+++ MiDA/src/ftl_gc.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cstdlib>
+#include "ssd_state.h"
+#include "ssd_config.h"
+#include "ftl.h"
+
+int pickVictimBlock() {
+    if (POLICY == GREEDY) {
+ 
+    int best_idx = 0;
+    int best_val = invalid_counter[0];
+
+    for (int i = 1; i < PPN_MAX; ++i) {
+        int v = invalid_counter[i];
+        if (v > best_val) {
+            best_val = v;
+            best_idx = i;
+        }
+    }
+
+    if (best_val == -1) {
+        std::cerr << "some error exists\n";
+        std::exit(EXIT_FAILURE);
+    }
+    VIC_NUM++;
+    return best_idx;
+}  
+ 	else {
+        std::cerr << "incorrect policy.\n";
+       	std::exit(EXIT_FAILURE);
+    }
+}
+
+void migrateValidPage(int src_ppn) {
+int lpn = PAGE_OOB[src_ppn].lpn;
+if (lpn == INVALID) return;
+
+if (LPN_TO_PPN[lpn] != src_ppn) {
+return;
+}
+write_lpn_gc(lpn);
+MIG_NUM++;}
+
+void erase(int bid) {
+    int base = FIRST_PPN_OF_BLOCK(bid);
+    for (int i = 0; i < PAGES_PER_BLOCK; ++i) {
+        int ppn = base + i;
+        PAGE_OOB[ppn].lpn = INVALID;
+        PAGE_OOB[ppn].state = PageState::erased;
+        PAGE_OOB[ppn].mig_count = 0;
+        DATA[ppn] = 0;
+    }
+    ERASE_NUM++;
+}
+
+void garbageCollection() {
+    int victim = pickVictimBlock();
+    int base = FIRST_PPN_OF_BLOCK(victim);
+
+    for (int i = 0; i < PAGES_PER_BLOCK; ++i) {
+        int ppn = base + i;
+        migrateValidPage(ppn);
+    }
+    erase(victim);
+    FREE_BLOCK_Q.push(victim);
+}
+
---
+++ MiDA/src/ftl_write.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "ssd_state.h"
+#include "ssd_config.h"
+#include "ftl.h"
+
+void write_lpn(int lpn) {
+    if (offset == 0) {
+        if (FREE_BLOCK_Q.empty()) {
+            std::cout << "No free blocks available!\n";
+            return;
+        }
+        current_block = FREE_BLOCK_Q.front();
+    }
+    if (LPN_TO_PPN[lpn] != INVALID) {
+    int page_num= LPN_TO_PPN[lpn];
+    int block_num=page_num/PAGES_PER_BLOCK;
+        ++invalid_counter[block_num];
+    }
+
+    int ppn = current_block * PAGES_PER_BLOCK + offset;
+    offset++;
+
+    LPN_TO_PPN[lpn] = ppn;
+    PAGE_OOB[ppn].lpn = lpn;
+    PAGE_OOB[ppn].state = PageState::valid;
+    DATA[ppn] = 1;
+
+    waf++;
+    waf2++;
+
+    if (offset == PAGES_PER_BLOCK) {
+        FREE_BLOCK_Q.pop();
+        offset = 0;
+    }
+}
+
+void write_lpn_gc(int lpn) {
+int i = (PAGE_OOB[page_num].mig_count < MIDA_n - 1) ? PAGE_OOB[page_num].mig_count + 1 : MIDA_n - 1;
+int ppn = MIDA_current_block[i] * PAGES_PER_BLOCK + MIDA_offset[i];
+PAGE_OOB[ppn].mig_count=i;
+    LPN_TO_PPN[lpn] = ppn;
+    PAGE_OOB[ppn].lpn = lpn;
+    PAGE_OOB[ppn].state = PageState::valid;
+    DATA[ppn] = 1;
+
+    waf++;
+    waf2++;
+
+    if (MIDA_offset[i] == PAGES_PER_BLOCK) {
+        MIDA_current_block[i]=FREE_BLOCK_Q.pop();
+        MIDA_offset[i] = 0;
+    }
+}
+
