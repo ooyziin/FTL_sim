41bbab24c763d25ea5c25ce2b835eb426999444b
ooyziin <2002byeon@naver.com>
2025-08-18 11:11:15 +0900

    Delete include directory

--- include/ftl.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include "ssd_config.h"
-
-int  pickVictimBlock();
-void migrateValidPage(int src_ppn);
-void erase(int bid);
-void garbageCollection();
-void write_lpn(int lpn);
--- include/trace.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <string>
-
-void processTraceFile(const std::string& filename);
